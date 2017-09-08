fdba80ef73aa37088dcb2dfc52ce1cf7d22f747a
Anna Kuć <nadannku@gmail.com>
2017-09-08 15:01:16 +0200

    trzy

---
+++ Warsztat6/src/main/java/pl/coderslab/warsztat6/AppConfig.java
@@ -0,0 +1,30 @@
+package pl.coderslab.warsztat6;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "pl.coderslab.controller", "pl.coderslab.entity" })
+@EnableTransactionManagement
+@EnableJpaRepositories("pl.coderslab.repository")
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
+}
